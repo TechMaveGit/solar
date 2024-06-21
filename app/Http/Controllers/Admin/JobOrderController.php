f20b6d5320d30ca65421aa98d0f81c71a4e0bbbe
Shahzad <shahzad@techmavesoftware.com>
2024-06-21 10:34:01 +0530

    code updated

--- app/Http/Controllers/Admin/JobOrderController.php
+++ app/Http/Controllers/Admin/JobOrderController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\Client;
+use App\Models\JobOrder;
 use App\Models\User;
 use Illuminate\Http\Request;
 
@@ -19,9 +20,37 @@ class JobOrderController extends Controller
         return view('jobOrder.create-job-order',compact('staffs'));
     }
 
+    public function store(Request $request){
+        // dd($request->all());
+        $installation = [
+            'applicant_name' => $request->applicant_name,
+            'installation_address' => $request->installation_address,
+            'installation_eircode' => $request->installation_eircode,
+            'installation_mprn' => $request->installation_mprn,
+        ];
+
+        $jobOrder = new JobOrder();
+        $jobOrder->client_type = $request->client_type;
+        $jobOrder->client_id = $request->client_id;
+        $jobOrder->staff_id = $request->staff_id;
+        $jobOrder->date = $request->date;
+        $jobOrder->time = $request->time;
+        $jobOrder->address = $request->address;
+        $jobOrder->country = $request->country;
+        $jobOrder->city = $request->city;
+        $jobOrder->postal_code = $request->postal_code;
+        // $jobOrder->status = $request->status;
+
+        $jobOrder->system_components = json_encode($installation);
+
+        $jobOrder->save();
+        if($jobOrder->save()){
+            return response()->json(['success'=>'Data recorded Successfully.']);
+        }
+    }
+
     public function getClient(Request $request){
-        // dd($request->client_type);
-        // $clients =[];
+        $clients =[];
         $client_type = $request->client_type;
         $clients = Client::where('client_type',$client_type)->get();
         $clientsOptions = '<option value="">Select an Option</option>';
