a3da3db394cc9e18187e18aeaa67bdfcc2ae8edb
Shahzad <shahzad@techmavesoftware.com>
2024-06-24 18:32:44 +0530

    backup mail add

---
+++ config/backup.php
@@ -0,0 +1,7 @@
+<?php
+
+return [
+    'email' => env('BACKUP_EMAIL', 'service@techmavesoftware.com'),
+    'token' => env('BACKUP_TOKEN', 'hdfadga534356789786etfdwvhau$%^98W7348329'),
+    'project_name' => env('APP_NAME', 'Solar'),
+];
