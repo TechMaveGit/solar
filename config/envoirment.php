b6f4d33032361d5d7da772771220a03b441905bb
Shahzad <shahzad@techmavesoftware.com>
2024-06-26 16:00:35 +0530

    Image URL add

--- config/envoirment.php
+++ config/envoirment.php
@@ -1,6 +1,6 @@
 <?php
 
 return [
-    'IMAGE_API_PATH' => 'https://techmavedev.com/solar/storage/',
+    'IMAGE_API_PATH' => 'https://techmavedev.com/solar/public/storage/',
     // other configuration variables...
 ];
